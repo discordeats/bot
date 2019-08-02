51466eba52f0fab7dad95cd0b5276e088d4ffcf6
Facto <33882218+Sniped@users.noreply.github.com>
2019-08-02 00:30:33 -0400

    react with check and x in suggestions

--- events/message.js
+++ events/message.js
@@ -4,4 +4,9 @@ module.exports = async (msg) => {
     if (msg.content.toLowerCase().trim().startsWith(client.config.prefix)) {
         await require('../modules/commandModule.js')(msg);
     }
+    
+    if (msg.channel.id == client.config.suggestionschannel) {
+        await msg.react('✅');
+        await msg.react('❌');
+    }
 }
